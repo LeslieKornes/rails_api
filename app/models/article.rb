a3759a922f665db22f6d1767faa8980bf7e7cb65
Leslie Kornes <lesliekornes@gmail.com>
2022-10-21 14:17:52 -0400

    add annotate gem

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :bigint           not null, primary key
+#  content    :text
+#  slug       :string
+#  title      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Article < ApplicationRecord
 end
